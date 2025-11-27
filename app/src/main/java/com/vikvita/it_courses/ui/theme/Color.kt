8382c617ae8bf639d74371ac67d2997caffde9cc
vikviita <vikvitadeveloper@gmail.com>
2025-11-27 22:11:33 +0500

    Настроена тема

--- app/src/main/java/com/vikvita/it_courses/ui/theme/Color.kt
+++ app/src/main/java/com/vikvita/it_courses/ui/theme/Color.kt
@@ -1,11 +1,45 @@
 package com.vikvita.it_courses.ui.theme
-
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val primaryLight = Color(0xFF12B956)
+val onPrimaryLight = Color(0xFFF2F2F3)
+val primaryContainerLight = Color(0xFFB6F1BB)
+val onPrimaryContainerLight = Color(0xFF1B5129)
+val secondaryLight = Color(0xFF24252A)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val secondaryContainerLight = Color(0xFFDCE1FF)
+val onSecondaryContainerLight = Color(0xFF354479)
+val tertiaryLight = Color(0xFF39656D)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val tertiaryContainerLight = Color(0xFFBDEAF3)
+val onTertiaryContainerLight = Color(0xFF1F4D55)
+val errorLight = Color(0xFFBA1A1A)
+val onErrorLight = Color(0xFFFFFFFF)
+val errorContainerLight = Color(0xFFFFDAD6)
+val onErrorContainerLight = Color(0xFF93000A)
+val backgroundLight = Color(0xFF151515)
+val onBackgroundLight = Color(0xFFF2F2F3)
+val surfaceLight = Color(0xFFF7FBF2)
+val onSurfaceLight = Color(0xFF181D18)
+val surfaceVariantLight = Color(0xFF24252A)
+val onSurfaceVariantLight = Color(0xFFF2F2F3)
+val outlineLight = Color(0x80F2F2F3)
+val outlineVariantLight = Color(0xFFC1C9BE)
+val scrimLight = Color(0xFF000000)
+val inverseSurfaceLight = Color(0xFF2D322C)
+val inverseOnSurfaceLight = Color(0xFFEEF2EA)
+val inversePrimaryLight = Color(0xFF9BD4A0)
+val surfaceDimLight = Color(0xFFD7DBD3)
+val surfaceBrightLight = Color(0xFFF7FBF2)
+val surfaceContainerLowestLight = Color(0xFFFFFFFF)
+val surfaceContainerLowLight = Color(0xFFF1F5EC)
+val surfaceContainerLight = Color(0xFFEBEFE7)
+val surfaceContainerHighLight = Color(0xFFE5E9E1)
+val surfaceContainerHighestLight = Color(0xFFE0E4DC)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+
+
+
+
+
